00ca793e6aa4a8712937cb42f555579d088b3949
miseon <miseon920@naver.com>
2022-07-07 14:26:45 +0900

    20220707

--- step04/js/step04.js
+++ step04/js/step04.js
@@ -1,1 +1,16 @@
-$(function () {});
+$(function () {
+  $(".mbtn").on("click", function () {
+    $(".gnb").toggleClass("on");
+  });
+  $(".gnb>ul>li>a").on("click", function (event) {
+    if ($(window).width() < 769) {
+      event.preventDefault();
+      $(".smenu").hide();
+      $(this).next().show();
+    }
+  });
+
+  $(window).on("resize", function () {
+    $(".smenu").removeAttr("style");
+  });
+});
--- step05/js/step05.js
+++ step05/js/step05.js
@@ -4,4 +4,36 @@ $(function () {
     autoplay: true,
     fade: true,
   });
+  //$(".mbtn").on("click", () => $(".gnb").toggleClass("on"));
+  $(".mbtn").click(function () {
+    $(".gnb").toggleClass("on");
+  });
+  const gnb_a = $("#header.on .gnb > ul > li a");
+  const w_w = $(window).width();
+  $(gnb_a).click(function () {
+    $(this)
+      .siblings(".smenu")
+      .stop()
+      .slideToggle()
+      .closest("li")
+      .siblings()
+      .find(".smenu")
+      .slideUp();
+    return false;
+  });
+  mob();
+  $(window).resize(function () {
+    mob();
+    $(".smenu").removeAttr("style");
+    $(".gnb").removeClass("on");
+  });
 });
+
+function mob() {
+  const w_w = $(window).width();
+  if (w_w > 767) {
+    $("#header").removeClass("on");
+  } else {
+    $("#header").addClass("on");
+  }
+}
